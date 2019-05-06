b27a1312bda605ec38e91f1bbbed92ad7319245c
Manuel Stoeckel <s3676959@stud.uni-frankfurt.de>
2019-05-06 14:40:03 +0200

    First Trail

---
+++ run_biofid.py
@@ -0,0 +1,58 @@
+import torch
+
+from flair.data import TaggedCorpus
+from flair.data_fetcher import NLPTaskDataFetcher
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, PooledFlairEmbeddings, ELMoEmbeddings, \
+    BertEmbeddings
+from typing import List
+
+print(torch.version.cuda)
+print("CUDA:", torch.cuda.current_device())
+device = torch.device('cuda:0')
+
+corpus: TaggedCorpus = NLPTaskDataFetcher.load_column_corpus(
+    'resources/corpora/biofid_28.03/',
+    {0: 'text', 1: 'pos', 2: 'lemma', 3: 'ner'},
+    train_file='train.biofid.conll',
+    test_file='test.biofid.conll',
+    dev_file='dev.biofid.conll')
+
+tag_type = 'ner'
+tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+
+# initialize embeddings
+embedding_types: List[TokenEmbeddings] = [
+
+    # GloVe embeddings
+    # WordEmbeddings('de'),
+    WordEmbeddings('/resources/public/stoeckel/emb/Leipzig1M.gensim'),
+
+    # ELMoEmbeddings(options_file='/resources/public/stoeckel/emb/ELMo_Leipzig100k/options_post.json',
+    #                weight_file='/resources/public/stoeckel/emb/ELMo_Leipzig100k/ELMo_Leipzig100k.hdf5'),
+
+    # BertEmbeddings('bert-base-multilingual-cased'),
+
+    # contextual string embeddings, forward
+    PooledFlairEmbeddings('german-forward', pooling='min'),
+
+    # contextual string embeddings, backward
+    PooledFlairEmbeddings('german-backward', pooling='min'),
+]
+
+embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+# initialize sequence tagger
+from flair.models import SequenceTagger
+
+tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+                                        embeddings=embeddings,
+                                        tag_dictionary=tag_dictionary,
+                                        tag_type=tag_type)
+
+# initialize trainer
+from flair.trainers import ModelTrainer
+
+trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+
+trainer.train('resources/taggers/biofid-ner',
+              max_epochs=150)
